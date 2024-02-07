c69125f8e3dc33f8263407340ae5fba502e1aa9f
Nico Rotili <64414877+NRotili@users.noreply.github.com>
2024-02-07 13:33:18 -0300

    act. 2024-02-07
    Modifico nº de filas por encabezado.

--- app/Imports/ProductosImport.php
+++ app/Imports/ProductosImport.php
@@ -5,43 +5,44 @@ namespace App\Imports;
 use App\Models\Product;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class ProductosImport implements ToCollection
+class ProductosImport implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
         foreach ($rows as $row){
         
             //Validate if product exist in table productos with codigo.
-            $producto = Product::where('codigo_producto', $row[7])->where('codigo_subproducto', $row[8])->first();
+            $producto = Product::where('codigo_producto', $row['codigoproducto'])->where('codigo_subproducto', $row['codigosubproducto'])->first();
             if ($producto) {
 
                 //If someone atributte is null, next producto
-                if ($row[1] == null || $row[5] == null || $row[6] == null || $row[7] == null ) {
+                if ($row['nombreproducto'] == null || $row['precioventa2'] == null || $row['precioventa3'] == null || $row['codigoproducto'] == null ) {
                     continue;
                 }
 
                 $producto->update([
-                    'nombre' => $row[1],
-                    'costo_minorista' => (float)$row[5],
-                    'costo_mayorista' => (float)$row[6],
-                    'codigo_producto' => $row[7],
-                    'codigo_subproducto' => $row[8],
+                    'nombre' => $row['nombreproducto'],
+                    'costo_minorista' => (float)$row['precioventa3'],
+                    'costo_mayorista' => (float)$row['precioventa2'],
+                    'codigo_producto' => $row['codigoproducto'],
+                    'codigo_subproducto' => $row['codigosubproducto'],
                 ]);
 
 
             } else {
 
-                if ($row[1] == null || $row[5] == null || $row[6] == null || $row[7] == null ) {
+                if ($row['nombreproducto'] == null || $row['precioventa2'] == null || $row['precioventa3'] == null || $row['codigoproducto'] == null ) {
                     continue;
                 }
                 
                 Product::create([
-                    'nombre' => $row[1],
-                    'costo_minorista' => (float)$row[5],
-                    'costo_mayorista' => (float)$row[6],
-                    'codigo_producto' => $row[7],
-                    'codigo_subproducto' => $row[8],
+                    'nombre' => $row['nombreproducto'],
+                    'costo_minorista' => (float)$row['precioventa3'],
+                    'costo_mayorista' => (float)$row['precioventa2'],
+                    'codigo_producto' => $row['codigoproducto'],
+                    'codigo_subproducto' => $row['codigosubproducto'],
                     'estado' => '0'
                 ]);
             }
