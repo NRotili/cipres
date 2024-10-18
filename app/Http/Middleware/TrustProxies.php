ad0dc84862aae8e8d1a0e1d5d636af761fef8117
Nico Rotili <64414877+NRotili@users.noreply.github.com>
2024-10-17 22:40:52 -0300

    fix(mixed-content)

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -5,15 +5,18 @@ namespace App\Http\Middleware;
 use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
-class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
      * @var array|string|null
      */
-    protected $proxies;
+    //protected $proxies;
 
+    protected $proxies = [
+                '104.237.3.91'  //replace with load balancer's public IP
+                    ];
+    ]
     /**
      * The headers that should be used to detect proxies.
      *
