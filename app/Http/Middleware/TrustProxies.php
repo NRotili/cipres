546517d16cf70e26417b275945c9d1f85a56811e
ROTNIC <rotilinicolas@gmail.com>
2024-10-19 20:29:25 -0300

    act. 2024-10-19

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,22 +1,16 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-
+class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
      * @var array|string|null
      */
-    //protected $proxies;
-
-    protected $proxies = [
-                '104.237.3.91'  //replace with load balancer's public IP
-                    ];
-    ]
+    protected $proxies;
     /**
      * The headers that should be used to detect proxies.
      *
